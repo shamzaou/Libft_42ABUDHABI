b4f3e76b2d7eec2d7db312c92e0f2592ed7fe566
shamzaou <shamzaou@student.42abudhabi.ae>
2022-09-25 09:29:34 +0400

    ft_strlcat fixed

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: shamzaou <shamzaou@student.42abudhabi.a    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 10:51:07 by shamzaou          #+#    #+#             */
-/*   Updated: 2022/09/16 09:14:44 by shamzaou         ###   ########.fr       */
+/*   Updated: 2022/09/25 09:28:38 by shamzaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_i;
 	size_t	src_i;
 
-	if (dstsize == 0)
-		return (0);
 	dst_i = 0;
 	src_i = 0;
+	if (dstsize == 0)
+		return (dst_i + ft_strlen(src));
 	while (dst[dst_i] && dst_i < dstsize)
 		dst_i++;
 	while (src[src_i] && (dst_i + src_i +1) < dstsize)
@@ -28,7 +28,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 		dst[dst_i + src_i] = src[src_i];
 		src_i++;
 	}
-	if (dst_i != dstsize)
+	if (dst_i < dstsize)
 		dst[dst_i + src_i] = '\0';
 	return (dst_i + ft_strlen(src));
 }
